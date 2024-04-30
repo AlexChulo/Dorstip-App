33e9230a1551a789406149ee2612f3564f9db3f7
AlexChulo <alex123149@gmail.com>
2024-04-30 22:27:20 +0200

    Design adjustments, Swap from SQLite to Firebase, working banner slider
    MySQL and SQLite were not working, changed location to a welcome to lessen the difficulty.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -33,20 +34,31 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures{
+        viewBinding=true
+    }
 }
 
 dependencies {
     implementation("androidx.viewpager2:viewpager2:1.0.0")
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.android.volley:volley:1.2.1")
     implementation("com.google.code.gson:gson:2.10.1")
-    implementation(files("libs\\mysql-connector-j-8.3.0.jar"))
+    implementation("com.google.firebase:firebase-database:20.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("com.google.android.gms:play-services-location:21.2.0")
     implementation ("androidx.cardview:cardview:1.0.0")
+
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation ("androidx.lifecycle:lifecycle-common-java8:2.7.0")
+    implementation ("androidx.activity:activity-ktx:1.9.0")
+    implementation("com.github.bumptech.glide:glide:4.13.0")
+    implementation("com.tbuonomo:dotsindicator:5.0")
 }
--- app/src/main/java/com/example/dorstip_app/dashboard/MainActivity.kt
+++ app/src/main/java/com/example/dorstip_app/dashboard/MainActivity.kt
@@ -2,18 +2,54 @@ package com.example.dorstip_app.dashboard
 
 
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.example.dorstip_app.R
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import com.example.dorstip_app.dashboard.BannerSlider.SliderAdapter
+import com.example.dorstip_app.dashboard.BannerSlider.SliderModel
+import com.example.dorstip_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel=MainViewModel()
+    private lateinit var binding:ActivityMainBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+            override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                binding= ActivityMainBinding.inflate(layoutInflater)
+                setContentView(binding.root)
+
+            initBanner()
+            }
+
+            private fun initBanner() {
+                viewModel.banners.observe(this, Observer {items->
+                banners(items)
+                })
+                viewModel.loadBanners()
+            }
+
+            private fun banners(images:List<SliderModel>){
+                binding.vpBanner.adapter= SliderAdapter(images,binding.vpBanner)
+                binding.vpBanner.clipToPadding=false
+                binding.vpBanner.clipChildren=false
+                binding.vpBanner.offscreenPageLimit=3
+                binding.vpBanner.getChildAt(0).overScrollMode=RecyclerView.OVER_SCROLL_NEVER
+
+                val compositePageTransformer=CompositePageTransformer().apply {
+                    addTransformer(MarginPageTransformer(40))
+                }
+                binding.vpBanner.setPageTransformer(compositePageTransformer)
+                if (images.size>1){
+                    binding.diBanner.visibility= View.VISIBLE
+                    binding.diBanner.attachTo(binding.vpBanner)
+                }
+            }
+        }
+
+
 
 
 
