f9050b9480cc59a59506a18cce97001445b812b3
AlexChulo <alex123149@gmail.com>
2024-05-05 15:00:08 +0200

    Working products, comments.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation("com.android.volley:volley:1.2.1")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.google.firebase:firebase-database:20.3.1")
+    implementation("androidx.activity:activity:1.8.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/dorstip_app/dashboard/Categories/CategoryAdapter.kt
+++ app/src/main/java/com/example/dorstip_app/dashboard/Categories/CategoryAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.dorstip_app.dashboard.Categories
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.ColorStateList
 import android.view.LayoutInflater
@@ -11,46 +12,59 @@ import com.bumptech.glide.Glide
 import com.example.dorstip_app.R
 import com.example.dorstip_app.databinding.ViewholderCategoryBinding
 
-class CategoryAdapter(val items:MutableList<CategoryModel>):
-    RecyclerView.Adapter<CategoryAdapter.Viewholder>() {
+class CategoryAdapter(val items: MutableList<CategoryModel>) :
+    RecyclerView.Adapter<CategoryAdapter.ViewHolder>() {
 
     private lateinit var context: Context
     private var selectedPosition = -1
-    private var lastSelectedPostion = -1
-    class Viewholder(val binding: ViewholderCategoryBinding) :
+    private var lastSelectedPosition = -1
+
+    // ViewHolder class to hold the views
+    class ViewHolder(val binding: ViewholderCategoryBinding) :
         RecyclerView.ViewHolder(binding.root) {
-
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryAdapter.Viewholder {
+    // onCreateViewHolder method to inflate the layout for each item
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         context = parent.context
-        val binding = ViewholderCategoryBinding.inflate(LayoutInflater.from(context), parent, false)
-        return Viewholder(binding)
+        val binding =
+            ViewholderCategoryBinding.inflate(LayoutInflater.from(context), parent, false)
+        return ViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: CategoryAdapter.Viewholder, position: Int) {
+    // onBindViewHolder method to bind data to each item's views
+    override fun onBindViewHolder(holder: ViewHolder, @SuppressLint("RecyclerView") position: Int) {
         val item = items[position]
         holder.binding.tvCategory.text = item.title
 
+        // Load image using Glide library
         Glide.with(holder.itemView.context)
             .load(item.picUrl)
             .into(holder.binding.ivCategory)
 
-        holder.binding.root.setOnClickListener{
-            lastSelectedPostion=selectedPosition
-            selectedPosition=position
+        // Click listener for each item
+        holder.binding.root.setOnClickListener {
+            lastSelectedPosition = selectedPosition
+            selectedPosition = position
+            // Notify adapter about data changes for the clicked item and the previously clicked item
             notifyItemChanged(selectedPosition)
-            notifyItemChanged(lastSelectedPostion)
+            notifyItemChanged(lastSelectedPosition)
         }
 
-        holder.binding.tvCategory.setTextColor(context.resources.getColor(R.color.accent))
-        if (selectedPosition==position){
-            holder.binding.layoutCategory.setBackgroundResource(R.drawable.category_background)
-            ImageViewCompat.setImageTintList(holder.binding.ivCategory, ColorStateList.valueOf(context.getColor(R.color.primary)))
+        // Change background and visibility based on item selection
+        if (selectedPosition == position) {
+            holder.binding.ivCategory.setBackgroundColor(0)
+            holder.binding.layoutCategory.setBackgroundResource(R.drawable.category_background2)
+            holder.binding.tvCategory.visibility = View.VISIBLE
+        } else {
+            holder.binding.ivCategory.setBackgroundResource(R.drawable.category_background)
+            holder.binding.layoutCategory.setBackgroundResource(0)
+            holder.binding.tvCategory.visibility = View.GONE
+        }
+    }
 
-            holder.binding.tvCategory.visibility=View.VISIBLE
-    }
-}
+    // getItemCount method to return the number of items in the list
     override fun getItemCount(): Int = items.size
-    }
+}
+
 
